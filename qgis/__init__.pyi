d735f40415a721b6cde81d05375b6af7b48c69ab
Leonhard S <leonhard-sei@outlook.com>
2022-09-14 14:38:28 +0200

    Add top-level module

---
+++ qgis/__init__.pyi
@@ -0,0 +1,12 @@
+from . import _3d, analysis, core, gui, processing, server
+
+__all__ = [
+    '_3d',
+    'analysis',
+    'core',
+    'gui',
+    'processing',
+    'server',
+]
+
+__version__ = '3.26.3'
